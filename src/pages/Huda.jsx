8c4c9a5bfa2de281ef5cabbc2129620d8535de3a
Fauzan <fauzantok076@gmail.com>
2026-04-23 22:07:12 +0700

    edit page idam

--- src/pages/Huda.jsx
+++ src/pages/Huda.jsx
@@ -9,7 +9,7 @@ function Huda() {
       kota="Kediri"
       jurusan="Teknologi Informasi"
       hobi="Belajar"
-      narasi="Cihuy"
+      narasi="Haloo... perkenalkan nama saya Mochamad Miftahul Huda, bisa dipanggil Idam atau Huda. Mohon doa dan restunya agar saya bisa sukses di dunia IT."
     />
   )
 }
